6ec6ee1c16443c19ce09f462d985ee27db473449
eric <emerabet@student.42.fr>
2018-09-25 18:12:27 +0200

    structure folders

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+
+
+const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
