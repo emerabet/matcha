c10e29d1da22905e21364fdef4ca10c453ed0393
eric <emerabet@student.42.fr>
2018-09-26 14:13:49 +0200

    server with cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4000;
 const bodyParser = require('body-parser');
@@ -12,13 +13,9 @@ const route = require('./routes/route');
 
 const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json({type: 'application/*+json'}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
