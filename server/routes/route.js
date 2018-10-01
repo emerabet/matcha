27270539dd458cb6dfe7c1bda3f8ae986499b0cf
eric <emerabet@student.42.fr>
2018-10-01 16:16:29 +0200

    delete useless route

--- server/routes/route.js
+++ server/routes/route.js
@@ -6,17 +6,4 @@ exports.setRoutes = (app) => {
     app.get('/', (req, res) => res.send('Hello World!'));
 
     app.post('/connect', db.login);
-
-    app.get('/check', (req, res) => {
-        console.log("check get");
-        
-        var token = req.headers['x-access-token'];
-
-        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-
-        jwt.verify(token, "config.secret", function(err, decoded) {
-            if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-            res.status(200).send(decoded);
-        });
-    });
 }
