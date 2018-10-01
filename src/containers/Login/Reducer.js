90c47b81b94197a7cd0a2a078bcef930c3d6013c
eric <emerabet@student.42.fr>
2018-10-01 16:13:29 +0200

    clean

--- src/containers/Login/Reducer.js
+++ src/containers/Login/Reducer.js
@@ -7,22 +7,20 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     let nextState;
     console.log('in login reducer');
-        switch(action.type) {
-            case actions.LOGIN:
-                console.log("from reducer", action);
-                console.log("token", action.data);
-                nextState = {...state,
-                            token: action.data};
-                break ;
-            case actions.LOGIN_FAIL:
-                nextState = {...state,
-                    token: null};
-                    
-                break ;
-                default:
-                    return state;
-        }
-        console.log(nextState);
+    switch(action.type) {
+        case actions.LOGIN:
+            console.log("from reducer", action);
+            console.log("token", action.data);
+            nextState = { ...state,
+                        token: action.data };
+            break ;
+        case actions.LOGIN_FAIL:
+            nextState = { ...state,
+                token: null };     
+            break ;
+        default: return state;
+    }
+    console.log(nextState);
     return nextState;
 }
 
