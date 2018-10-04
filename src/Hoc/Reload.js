9692a02c402770259c49deaed4d6ae14a5f1200c
Furyou81 <lfujimot@student.42.fr>
2018-10-04 19:07:24 +0200

    Hoc

--- src/Hoc/Reload.js
+++ src/Hoc/Reload.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from './Actions';
+import axios from 'axios';
 
 class Reload extends Component {
 
   
 
-    componentDidMount(){
-        if (this.props.user === undefined){
+    constructor(props){
+        super(props);
+     /*   const res = await axios.post('/connect', { token: sessionStorage.getItem('token') });
+            console.log("actions login");
+            console.log('data login', res.data);*/
+            console.log("PROPS", props);
+            if (this.props.user === undefined){
             this.props.onRestoreStore();
         } else {
             console.log("NO NEED TO RESTORE");
