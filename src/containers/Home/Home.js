18f942edb2e1ba22366180f6988277fb887fe571
eric <emerabet@student.42.fr>
2018-10-11 15:43:02 +0200

    suppréssion material ui

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,11 +4,6 @@ import { connect } from 'react-redux';
 import TopMenu from '../../components/Menu/TopMenu';
 import classes from './Home.css';
 import './Home.css';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 
 class Home extends Component {
@@ -20,16 +15,7 @@ class Home extends Component {
     const { activeItem } = this.state
 
     return (
-      <BottomNavigation
-            value={5}
-            onChange={null}
-            showLabels
-            className={classes.root}
-          >
-            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
+      <div>Empty</div>
     )
   }
 
