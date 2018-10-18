5427c0413d7c74f7e8d0cf118c97b46c1ad42be8
eric <emerabet@student.42.fr>
2018-10-18 13:18:16 +0200

    indent

--- server/socket.js
+++ server/socket.js
@@ -16,17 +16,15 @@ const mySocket = async (socket, connectedUsers) => {
 	socket.on('login', async (data) => {
 		const c = parseCookies(socket.request.headers.cookie);
 		const token = c.get('sessionid');
-    try {
-        const decoded = await jwt.verify(token, config.SECRET_KEY);
-        if (decoded.err)
-            return ;
-        else {
-			connectedUsers.push(data);
-			console.log("LIST OF CONNECTED USERS", connectedUsers);
-        }
-    } catch (err) {
-        return ;
-    }
+		try {
+			const decoded = await jwt.verify(token, config.SECRET_KEY);
+			if (decoded.err)
+				return ;
+			else {
+				connectedUsers.push(data);
+				console.log("LIST OF CONNECTED USERS", connectedUsers);
+			}
+		} catch (err) { return ; }
 	});
 };
 
