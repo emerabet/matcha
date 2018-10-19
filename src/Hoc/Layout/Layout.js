8871d6e2fab76522492d4ffd24465b25451ea8ca
eric <emerabet@student.42.fr>
2018-10-19 15:17:26 +0200

    layout connected to the store and socket

--- src/Hoc/Layout/Layout.js
+++ src/Hoc/Layout/Layout.js
@@ -5,10 +5,37 @@ import { connect } from 'react-redux';
 import Aux from '../Aux/Aux';
 import TopMenu from '../../components/Menu/TopMenu';
 import SuperChat from '../../containers/SuperChat/SuperChat';
+import withSocket from '../Socket/SocketHOC';
+
+import * as actionsActivity from '../../components/Activity/Actions';
+
 import './Layout.css';
 
 
-class Stalk extends Component {
+class Layout extends Component {
+
+    componentDidMount() {
+
+        this.props.socket.on('visited', (data) => {
+
+            console.log(data);
+            this.props.onLoadNotifications('all');
+        });
+
+        this.props.socket.on('liked', (data) => {
+
+            console.log(data);
+            this.props.onLoadNotifications('all');
+        });
+
+        this.props.socket.on('unliked', (data) => {
+
+            console.log(data);
+            this.props.onLoadNotifications('all');
+        });
+
+
+    }
 
     render() {
         return (
@@ -24,4 +51,14 @@ class Stalk extends Component {
 
 }
 
-export default Stalk;
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLoadNotifications: (type) => dispatch(actionsActivity.load(type)),
+    }
+}
+
+
+export default withSocket(connect(mapStateToProps, mapDispatchToProps)(Layout));
