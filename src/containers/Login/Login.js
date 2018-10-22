57933101bdf6bb256e17451f41bb4fa99093ee5e
eric <emerabet@student.42.fr>
2018-10-22 14:47:35 +0200

    logo

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -33,6 +33,7 @@ class  Login extends Component {
     render (){
         return (
             <div className='Login_Register__Container'>
+                
                 <Form className='Login_Register__Form' onSubmit={this.handleLogin}>
                     <Form.Field>
                         <label>Username</label>
